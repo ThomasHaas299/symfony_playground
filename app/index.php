da1745c20a1eda49af9293955472a0f84ba757e9
Thomas Haas <github@thomashaas.de>
2024-12-28 17:29:36 +0100

    composer example

--- app/index.php
+++ app/index.php
@@ -1,3 +1,20 @@
-<?php
+Im Container symfony-php wurde eine bash geöffnet
+<pre>
+    docker exec -it symfony-php bash
+</pre>
+Dort wurde dann folgendes ausgeführt:
+<pre>
+    composer require nesbot/carbon
+</pre>
+Hinweis: Es hat etliche symfony-Abhängigkeiten.<br>
+Und das wird im Folgenden verwendet.
+<hr>
 
-phpinfo();
+<?php
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+$now = Carbon::now();
+echo "Aktuelles Datum: " . $now->toDateString() . "<br>";
+echo "Vor einer Woche: " . $now->subWeek()->toDateString();
