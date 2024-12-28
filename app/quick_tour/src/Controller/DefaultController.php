20b6d8090bf67ccaaffddf8399ea5f794e4f5e55
Thomas Haas <github@thomashaas.de>
2024-12-28 18:36:32 +0100

    Ein bisschen Routing

--- app/quick_tour/src/Controller/DefaultController.php
+++ app/quick_tour/src/Controller/DefaultController.php
@@ -13,4 +13,24 @@ class DefaultController
     {
         return new Response("Hello $name!");
     }
+
+    #[Route('/simplicity', methods: ['GET'])]
+    public function simple(): Response
+    {
+        return new Response('Simple! Easy! Great!');
+    }
+
+    // default route
+    #[Route('/', name: 'default')]
+    public function default(): Response
+    {
+        return new Response("Hello (default)!");
+    }
+
+    // any other route
+    #[Route('/{any}', name: 'any', requirements: ['any' => '.*'])]
+    public function any(string $any): Response
+    {
+        return new Response("Erm... $any?");
+    }
 }
