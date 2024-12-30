88aec97a94fb42000249a2f5e8e9d8cf8d6eefcf
Thomas Haas <github@thomashaas.de>
2024-12-30 16:43:11 +0100

    filesystem_trial bestanden

--- app/index.php
+++ app/index.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+use Symfony\Component\Filesystem\Filesystem;
+
+require __DIR__ . '/vendor/autoload.php';
+
+const NL = '<br>';
+
+$filesystem = new Filesystem();
+
+echo 'Es gibt die Datei index.html: ' . ($filesystem->exists('index.html') ? 'Ja' : 'Nein'). NL;
+echo 'Die Datei enthält '.strlen($filesystem->readFile('index.html')).' Zeichen'. NL;
